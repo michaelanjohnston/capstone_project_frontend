ed5dcfaec9819ced5289dc5711a00b98df076237
MICHAEL JOHNSTON <MICHAEL.JOHNSTON.A@GMAIL.COM>
2021-07-04 13:03:19 -0700

    Added login and logout features

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import SavedQuotesIndex from "../views/SavedQuotesIndex.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter);
 
@@ -20,6 +23,9 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   { path: "/saved_quotes", name: "saved_quotes-index", component: SavedQuotesIndex },
+  { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
+  { path: "/logout", name: "logout", component: Logout },
 ];
 
 const router = new VueRouter({
